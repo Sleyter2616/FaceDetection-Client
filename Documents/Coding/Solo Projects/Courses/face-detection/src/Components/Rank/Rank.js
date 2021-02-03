9022a441579aa0876edcbd244ed4f6026f21fc88
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-02-03 15:07:46 -0500

    main file has up to date code .

---
+++ Documents/Coding/Solo Projects/Courses/face-detection/src/Components/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from "react"
+const Rank = () =>{
+    return(
+      <div >
+       <div className='white f3'> 
+       {'Sleyter, your current Rank is...'}
+       </div>
+       <div className='white f1'>
+           {'#1'}
+       </div>
+      </div>
+    );
+}
+export default Rank
